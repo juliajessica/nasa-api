0ad35d1b210a6f8f89f938b7563771d43736dbf6
julia sheremet <juliajessicasher@gmail.com>
2018-05-10 10:35:43 -0700

    create a method to return the date and time from the api

--- src/app/rover-form/rover-form.component.ts
+++ src/app/rover-form/rover-form.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { MarsRoverApiService } from '../mars-rover-api.service'
 
 @Component({
   selector: 'app-rover-form',
   templateUrl: './rover-form.component.html',
-  styleUrls: ['./rover-form.component.css']
+  styleUrls: ['./rover-form.component.css'],
+  providers: [ MarsRoverApiService ]
 })
-export class RoverFormComponent implements OnInit {
+export class RoverFormComponent {
+  photos: any[]=null;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private marsRoverPhotos: MarsRoverApiService) { }
+  getRoverImages(date: string, camera: string){
+    this.marsRoverPhotos.getByDateAndCamera(date, camera).subscribe(response => { //getByDateAndCamera this method is grabbing the info from the api
+      this.photos = response.json(); //returning the marsRoverPhotos and naming it photos
+    });
   }
-
 }
