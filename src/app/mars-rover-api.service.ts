efa9e61b010f84fbcd530a12189a37cf3cf88a40
julia sheremet <juliajessicasher@gmail.com>
2018-05-10 11:04:23 -0700

    add NASA api key and populate images on page

--- src/app/mars-rover-api.service.ts
+++ src/app/mars-rover-api.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { marsRoverKey } from './api-key';
 
 @Injectable()
 export class MarsRoverApiService {
   constructor(private http: Http){}//private means its only accessable in this instance
 
   getByDateAndCamera(date: string, camera: string) {
-  return this.http.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&camera=${camera}&&api_key=DEMO_KEY`)
+  return this.http.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&camera=${camera}&&api_key=${marsRoverKey}`)
 }
 
 }
